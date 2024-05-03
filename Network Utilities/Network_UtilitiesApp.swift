7209e8c9c9eb013343fd83ac5b07f6108a2c88cb
rngkGit <Kehaw06@gmail.com>
2024-05-03 13:44:31 -0500

    Initial Commit

---
+++ Network Utilities/Network_UtilitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Network_UtilitiesApp.swift
+//  Network Utilities
+//
+//  Created by Keith Beavers on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Network_UtilitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
